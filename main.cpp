add2013e83ef95f5b39a5f49ceacba0b95a2a65c
Bryan Romero <bryanromerou@gmail.com>
2020-12-19 10:25:40 -0800

    learning c++ primitive types

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main (){
+    cout << "Hello World" << endl;
+
+    // We make Const all uppercase
+    const double PI = 3.1415926535;
+    
+    // Chars need to be in single quotations
+    char myGrade = 'A';
+
+    // Primitive Data Types.
+    bool isHappy = true;
+    int age = 24;
+    float favNum = 7.89;
+    double otherFavNum = 56.922233;
+
+    cout << myGrade << endl;
+    cout << "Favorite number is: " << favNum << endl;
+    return 0;
+}
